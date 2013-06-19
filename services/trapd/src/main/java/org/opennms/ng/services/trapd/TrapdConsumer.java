613d367ad1419e4f24326680e34247270be58786
Jeff Genender <jgenender@savoirtech.com>
2013-06-19 17:35:03 -0400

    Trapd code

--- services/trapd/src/main/java/org/opennms/ng/services/trapd/TrapdConsumer.java
+++ services/trapd/src/main/java/org/opennms/ng/services/trapd/TrapdConsumer.java
@@ -1,25 +1,51 @@
 package org.opennms.ng.services.trapd;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.impl.DefaultConsumer;
+import org.apache.camel.impl.DefaultExchange;
+import org.opennms.netmgt.snmp.TrapNotification;
 
-public class TrapdConsumer extends DefaultConsumer{
+public class TrapdConsumer extends DefaultConsumer {
 
     private final TrapdEndpoint endpoint;
+    private final Processor processor;
+    private Trapd trapd;
 
     public TrapdConsumer(Endpoint endpoint, Processor processor) {
         super(endpoint, processor);
         this.endpoint = (TrapdEndpoint) endpoint;
+        this.processor = processor;
+
+        TrapdConfiguration config = ((TrapdEndpoint) endpoint).getConfig();
+
+        trapd = new Trapd(this);
+        trapd.setTrapdIpMgr(config.getTrapdIpMgr());
+        trapd.setSnmpTrapAddress(config.getSnmpTrapAddress());
+        trapd.setSnmpTrapPort(config.getSnmpTrapPort());
+        trapd.setSnmpV3Users(config.getSnmpV3Users());
+
+        trapd.onInit();
+    }
+
+    public void onTrap(TrapNotification trapNotification) throws Exception {
+
+        Exchange exchange = new DefaultExchange(endpoint);
+        exchange.getIn().setBody(trapNotification);
+
+        processor.process(exchange);
     }
 
     @Override
     protected void doStop() throws Exception {
-        super.doStop();    //To change body of overridden methods use File | Settings | File Templates.
+        trapd.onStop();
+        super.doStop();
     }
 
     @Override
     protected void doStart() throws Exception {
-        super.doStart();    //To change body of overridden methods use File | Settings | File Templates.
+        trapd.onStart();
+        super.doStart();
     }
 }
