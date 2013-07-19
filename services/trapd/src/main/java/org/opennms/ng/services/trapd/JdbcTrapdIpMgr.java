c0e37a6cdaf4015d72eee6560d1e1e276dd800f2
Johan Edstrom <jedstrom@savoirtech.com>
2013-07-19 11:23:18 -0600

    Skip bad looking for ip.

--- services/trapd/src/main/java/org/opennms/ng/services/trapd/JdbcTrapdIpMgr.java
+++ services/trapd/src/main/java/org/opennms/ng/services/trapd/JdbcTrapdIpMgr.java
@@ -1,15 +1,11 @@
 package org.opennms.ng.services.trapd;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
 
 import org.opennms.netmgt.trapd.TrapdIpMgr;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.util.Assert;
 
 /**
@@ -22,13 +18,11 @@ import org.springframework.util.Assert;
 public class JdbcTrapdIpMgr implements TrapdIpMgr, InitializingBean {
 
     private DataSource m_dataSource;
-
     /**
      * The SQL statement used to extract the list of currently known IP
      * addresses and their node IDs from the IP Interface table.
      */
-    private final  String IP_LOAD_SQL = "SELECT ipAddr, nodeid FROM ipInterface";
-
+    private final String IP_LOAD_SQL = "SELECT ipAddr, nodeid FROM ipInterface";
     /**
      * A Map of IP addresses and node IDs
      */
@@ -43,30 +37,41 @@ public class JdbcTrapdIpMgr implements TrapdIpMgr, InitializingBean {
     /* (non-Javadoc)
      * @see org.opennms.netmgt.trapd.TrapdIpMgr#dataSourceSync()
      */
-    /**
-     * <p>dataSourceSync</p>
-     */
 
-
-    @Override
-    public synchronized void dataSourceSync() {
-        m_knownips.clear();
-
-
-        new JdbcTemplate(m_dataSource).query(IP_LOAD_SQL, new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                m_knownips.put(rs.getString(1), rs.getLong(2));
-            }
-        });
-
-
+    private static long longValue(Long result) {
+        return (result == null ? -1 : result.longValue());
     }
 
     /* (non-Javadoc)
      * @see org.opennms.netmgt.trapd.TrapdIpMgr#getNodeId(java.lang.String)
      */
-    /** {@inheritDoc} */
+
+    /**
+     * <p>dataSourceSync</p>
+     */
+
+    @Override
+    public synchronized void dataSourceSync() {
+        m_knownips.clear();
+
+        //TODO - INJECT TEMPLATE WITH RIGHT DS
+        /**
+         new JdbcTemplate(m_dataSource).query(IP_LOAD_SQL, new RowCallbackHandler() {
+        @Override public void processRow(ResultSet rs) throws SQLException {
+        m_knownips.put(rs.getString(1), rs.getLong(2));
+        }
+        });
+         */
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.trapd.TrapdIpMgr#setNodeId(java.lang.String, long)
+     */
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public synchronized long getNodeId(String addr) {
         if (addr == null) {
@@ -76,9 +81,12 @@ public class JdbcTrapdIpMgr implements TrapdIpMgr, InitializingBean {
     }
 
     /* (non-Javadoc)
-     * @see org.opennms.netmgt.trapd.TrapdIpMgr#setNodeId(java.lang.String, long)
+     * @see org.opennms.netmgt.trapd.TrapdIpMgr#removeNodeId(java.lang.String)
      */
-    /** {@inheritDoc} */
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public synchronized long setNodeId(String addr, long nodeid) {
         if (addr == null || nodeid == -1) {
@@ -89,9 +97,12 @@ public class JdbcTrapdIpMgr implements TrapdIpMgr, InitializingBean {
     }
 
     /* (non-Javadoc)
-     * @see org.opennms.netmgt.trapd.TrapdIpMgr#removeNodeId(java.lang.String)
+     * @see org.opennms.netmgt.trapd.TrapdIpMgr#clearKnownIpsMap()
      */
-    /** {@inheritDoc} */
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public synchronized long removeNodeId(String addr) {
         if (addr == null) {
@@ -100,19 +111,12 @@ public class JdbcTrapdIpMgr implements TrapdIpMgr, InitializingBean {
         return longValue(m_knownips.remove(addr));
     }
 
-    /* (non-Javadoc)
-     * @see org.opennms.netmgt.trapd.TrapdIpMgr#clearKnownIpsMap()
-     */
     /**
      * <p>clearKnownIpsMap</p>
      */
     @Override
     public synchronized void clearKnownIpsMap() {
         m_knownips.clear();
-    }
-
-    private static long longValue(Long result) {
-        return (result == null ? -1 : result.longValue());
     }
 
     /**
